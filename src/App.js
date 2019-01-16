e579186de7a66885261539971b299175cca095f6
Jeannie Kim <SJ.Jeannie.Kim@gmail.com>
2019-01-15 20:04:23 -0600

    WarpCable Subscribe added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 // import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import WarpCable from 'warp-cable-client'
-import { Login } from './views/Login.js'
-import { Home } from './views/Home.js'
-import { Signup } from './views/Signup.js'
-const API_DOMAIN = 'ws://localhost:3000/cable'
-let api = WarpCable(API_DOMAIN)
-window.api = api
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import WarpCable from 'warp-cable-client';
+import { Login } from './views/Login.js';
+import { Home } from './views/Home.js';
+import { Signup } from './views/Signup.js';
+const API_DOMAIN = 'ws://localhost:3000/cable';
+let api = WarpCable(API_DOMAIN);
+window.api = api;
+let controllers = ['Users', 'Invites', 'Messages', 'Meals'];
 class App extends Component {
+  setSubscribe = (controllerArray) => {
+    controllerArray.forEach((controller) =>
+      api.subscribe(
+        controller,
+        'index',
+        { Authorization: `BEARER ${localStorage.token}` },
+        (users) => {
+          console.log('Received:', users);
+        }
+      )
+    );
+  };
   render() {
-    
+    this.setSubscribe(controllers);
     return (
       <BrowserRouter>
         <Switch>
---
+++ src/views/Home.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import WarpCable from 'warp-cable-client';
+const API_DOMAIN = 'ws://locahost:3000/cable';
+let api = WarpCable(API_DOMAIN);
+let controllers = ['Users', 'Invites', 'Messages', 'Meals'];
+
+export class Home extends React.Component {
+  state = {
+    user: null
+  };
+
+  fetchUserInfo = () => {
+    fetch(`http://localhost:3000/users/${localStorage.userID}`)
+      .then((res) => res.json())
+      .then((userInfo) => this.setState({ user: { ...userInfo } }));
+  };
+
+  // api.trigger('Users', 'update', {id: 1, email: "test3", password:"123", first_name:"Jordan!", last_name:"Laird", Authorization: `BEARER ${localStorage.token}`}, console.log)
+
+  componentDidMount() {
+    this.fetchUserInfo();
+  }
+
+  setSubscribe = (controllerArray) => {
+    controllerArray.forEach((controller) =>
+      api.subscribe(
+        controller,
+        'index',
+        { Authorization: `BEARER ${localStorage.token}` },
+        (users) => {
+          console.log('Received:', users);
+        }
+      )
+    );
+  };
+  render() {
+    this.setSubscribe(controllers);
+    if (this.state.user) {
+      return <Container>{this.state.user.first_name}</Container>;
+    } else {
+      return null;
+    }
+  }
+}
